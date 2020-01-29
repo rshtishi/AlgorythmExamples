cc16e68918654686d115f2fb0b277c52278dd8b3
rshtishi <rshtishi@3dial.eu>
2020-01-29 15:21:51 +0100

    adding some algorythm

--- src/main/java/al/tirana/arrays/ArrayUtil.java
+++ src/main/java/al/tirana/arrays/ArrayUtil.java
@@ -5,46 +5,58 @@ import java.util.BitSet;
 
 public class ArrayUtil {
 
-	
 	public static int[] findMissingNumbers(int[] numbers, int arraySize) {
-		int missingArraySize = arraySize - numbers.length; 
+		int missingArraySize = arraySize - numbers.length;
 		int[] missingNumbers = new int[missingArraySize];
-		
-		BitSet bitSet = new BitSet(arraySize); 
-		for (int number : numbers) { 
-			bitSet.set(number - 1); 
-		} 
-	
+
+		BitSet bitSet = new BitSet(arraySize);
+		for (int number : numbers) {
+			bitSet.set(number - 1);
+		}
+
 		int lastMissingIndex = 0;
 		for (int i = 0; i < missingArraySize; i++) {
-			lastMissingIndex = bitSet.nextClearBit(lastMissingIndex); 
+			lastMissingIndex = bitSet.nextClearBit(lastMissingIndex);
 			lastMissingIndex++;
 			missingNumbers[i] = lastMissingIndex;
 		}
 		return missingNumbers;
 	}
-	
-	
+
 	public static int[] findDuplicateNumbers(int[] numbers) {
-		int duplicateNumbersSize = numbers.length/2;
+		int duplicateNumbersSize = numbers.length / 2;
 		int[] duplicateNumbers = new int[duplicateNumbersSize];
 		int index = 0;
 		Arrays.sort(numbers);
-		for(int i=0; i<numbers.length-1;) {
-			int j=i+1;
-			while(numbers[i]==numbers[j]){
-				if(j-i==1) {
-					duplicateNumbers[index]=numbers[i];
+		for (int i = 0; i < numbers.length - 1;) {
+			int j = i + 1;
+			while (numbers[i] == numbers[j]) {
+				if (j - i == 1) {
+					duplicateNumbers[index] = numbers[i];
 					index++;
 				}
 				j++;
 			}
-			i=j;
+			i = j;
 		}
 		return duplicateNumbers;
-
 	}
-	
+
+	public static int[] findMaxAndMinNumbers(int[] numbers) {
+		int maxNum = numbers[0];
+		int minNum = numbers[0];
+		for (int i = 1; i < numbers.length; i++) {
+			if (numbers[i] > maxNum) {
+				maxNum = numbers[i];
+			} else if (numbers[i] < minNum) {
+				minNum = numbers[i];
+			}
+		}
+		int[] extremeArr = new int[2];
+		extremeArr[0] = maxNum;
+		extremeArr[1] = minNum;
+		return extremeArr;
+	}
 	
 
 }
--- src/test/java/al/tirana/arrays/ArrayUtilTest.java
+++ src/test/java/al/tirana/arrays/ArrayUtilTest.java
@@ -2,32 +2,42 @@ package al.tirana.arrays;
 
 import static org.junit.Assert.assertArrayEquals;
 
-import java.util.Arrays;
-
 import org.junit.Test;
 
 public class ArrayUtilTest {
 
 	@Test
 	public void testFindMissingNumbers() {
-		//setup 
-		int[] numbers = new int[] {1, 2, 3, 4, 6, 9, 8};
+		// setup
+		int[] numbers = new int[] { 1, 2, 3, 4, 6, 9, 8 };
 		int arraySize = 10;
-		//execute
-		int[] missingNumbers = ArrayUtil.findMissingNumbers(numbers,arraySize);
-		//verify
-		int[] expected = new int[] {5, 7, 10};
+		// execute
+		int[] missingNumbers = ArrayUtil.findMissingNumbers(numbers, arraySize);
+		// verify
+		int[] expected = new int[] { 5, 7, 10 };
 		assertArrayEquals(expected, missingNumbers);
 	}
-	
+
 	@Test
-	public void testfindDuplicateNumbers() {
-		//setup 
-		int[] numbers = new int[] {1, 1, 2, 2, 3, 3, 5};
-		//execute
+	public void testFindDuplicateNumbers() {
+		// setup
+		int[] numbers = new int[] { 1, 1, 2, 2, 3, 3, 5 };
+		// execute
 		int[] duplicateNumbers = ArrayUtil.findDuplicateNumbers(numbers);
+		// verify
+		int[] expected = new int[] { 1, 2, 3 };
+		assertArrayEquals(expected, duplicateNumbers);
+	}
+
+	@Test
+	public void testfindMaxAndMinNumbers() {
+		// setup
+		int[] numbers = new int[] { -2, 14, 21, -8, 12};
+		//execute
+		int[] extremesArr = ArrayUtil.findMaxAndMinNumbers(numbers);
 		//verify
-		System.out.println(Arrays.toString(duplicateNumbers));
+		int[] expected = new int[] { 21, -8};
+		assertArrayEquals(expected, extremesArr);
 	}
 
 }
