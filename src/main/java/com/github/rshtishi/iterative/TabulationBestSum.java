52ca84779df64115f2aa15d09e42be022067287d
Rando Shishi <rando.shtishi@gmail.com>
2021-05-04 20:14:35 +0200

    Fixed the tabulation solution of the best sum problem

--- src/main/java/com/github/rshtishi/iterative/TabulationBestSum.java
+++ src/main/java/com/github/rshtishi/iterative/TabulationBestSum.java
@@ -6,20 +6,22 @@ import java.util.List;
 public class TabulationBestSum {
 
 	public static List<Integer> compute(int num, int[] values) {
-		List<List<Integer>> list = new ArrayList<>(num+1);
+		List<List<Integer>> list = new ArrayList<>(num + 1);
 		list.add(new ArrayList<Integer>());
-		for(int i=1;i<=num;i++) {
+		for (int i = 1; i <= num; i++) {
 			list.add(null);
 		}
 		for (int i = 0; i < num; i++) {
 			if (list.get(i) != null) {
 				for (int value : values) {
 					int index = i + value;
-					List<Integer> combination = new ArrayList<>();
-					combination.addAll(list.get(i));
-					combination.add(value);
-					if (list.get(index) == null || list.get(index).size() > combination.size()) {
-						list.set(index, combination);
+					if (index < num+1) {
+						List<Integer> temp = new ArrayList<>();
+						temp.addAll(list.get(i));
+						temp.add(value);
+						if (list.get(index) == null || list.get(index).size() > temp.size()) {
+							list.set(index, temp);
+						}
 					}
 				}
 			}
@@ -27,9 +29,9 @@ public class TabulationBestSum {
 		return list.get(num);
 	}
 
-	public static void main (String[] args) {
-		int[] test=new int[] { 3, 5, 7, 8 };
-		TabulationBestSum.compute(8,test);
+	public static void main(String[] args) {
+		int[] test = new int[] { 2, 5 };
+		System.out.println(TabulationBestSum.compute(10, test));
 	}
 
 }
