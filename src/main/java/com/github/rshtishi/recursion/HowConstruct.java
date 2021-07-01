d0ca93d2f2959a78f5d790020a9844614ea1fb29
Rando Shishi <rando.shtishi@gmail.com>
2021-07-01 22:19:16 +0200

    HowContruct Problem

---
+++ src/main/java/com/github/rshtishi/recursion/HowConstruct.java
@@ -0,0 +1,11 @@
+package com.github.rshtishi.recursion;
+
+import java.util.List;
+
+public class HowConstruct {
+	
+	public static List<String> compute(String target, String[] values){
+		return null;
+	}
+
+}
---
+++ src/test/java/com/github/rshtishi/recursion/HowConstructTest.java
@@ -0,0 +1,36 @@
+package com.github.rshtishi.recursion;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+public class HowConstructTest {
+
+	@Rule
+	public Timeout globalTimeout = Timeout.seconds(1);
+
+	public static final Object[] getTestData() {
+		return new Object[] {
+				new Object[] { "Rando", new String[] { "Ra", "do", "n", "Rand" }, Arrays.asList( "Ra", "n", "do" ) },
+				new Object[] { "Rando", new String[] { "o", "do", "n", "Rand" }, Arrays.asList( "Rand", "o" ) },
+				new Object[] { "Tirana", new String[] { "Ti", "ran", "na", "rana" }, Arrays.asList("Ti", "rana" ) },
+				new Object[] { "Tirana", new String[] { "Tir", "rana", "na", "ir" }, null },
+				new Object[] { "Rando", new String[] { "Ran", "and", "ndo" }, null } };
+
+	}
+
+	@Test
+	@Parameters(method = "getTestData")
+	public void test(String target, String[] wordBank,List<String> result) {
+		HowConstruct.compute(target, wordBank);
+	}
+
+}
